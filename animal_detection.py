288ea763c83a7906d39a25c5dc86cc72a69c5677
Michael Eggers <mseggers@protonmail.ch>
2021-12-06 00:50:53 +0100

    cleanup old code. draw confidence. use zip() for iteration

--- animal_detection.py
+++ animal_detection.py
@@ -26,30 +26,18 @@ while True:
     ret, frame = videoCapture.read()
     smallFrame = frame # cv2.resize(frame, (0, 0), fx=1.0, fy=1.0)
 
-    classIDs, confidence, bboxArray = model.detect(smallFrame, confThreshold=0.6, nmsThreshold=0.2)
+    classIDs, confidences, bboxArray = model.detect(smallFrame, confThreshold=0.6, nmsThreshold=0.1)
 
-    # print("length classIDs: ", len(classIDs))
-    # print("length bbox: ", len(bbox))
-    # print(len(classIDs) == len(bbox))
-    for i, classID in enumerate(classIDs):
-        # p2 = (bbox[0], bbox[1])
-        # print(bbox)
-        bbox = bboxArray[i]
-        p1 = (bbox[0], bbox[1])
-        p2 = (bbox[2], bbox[3])
-        name = classNames[classID - 1]
-        cv2.rectangle(smallFrame, bbox, color=(255, 0, 0), thickness=2)
-        cv2.putText(smallFrame, name, p1, cv2.FONT_HERSHEY_COMPLEX, 1.0, (255, 255, 255), 2)
-        print("Confidence: " + name, confidence[i])
-        # if classID == 19:
-        #     cv2.putText(smallFrame, "Horse!", (200, 200), 1, 3.0, (244, 244, 244))
-        # elif classID == 20:
-        #     cv2.putText(smallFrame, "Sheep!", (200, 200), 1, 3.0, (244, 244, 244))
-
-            # for box in bbox:
-            #     p1 = (box[2], box[3])
-            #     p2 = (box[0], box[1])
-            #     cv2.rectangle(smallFrame, p1, p2, (0, 255, 255))
+    for classID, confidence, bbox in zip(classIDs, confidences, bboxArray):
+        if confidence > 0.4:
+            p1 = (bbox[0], bbox[1])
+            p2 = (bbox[2], bbox[3])
+            name = classNames[classID - 1]
+            cv2.rectangle(smallFrame, bbox, color=(255, 0, 0), thickness=2)
+            cv2.putText(smallFrame, name, p1, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+            cv2.putText(smallFrame, str(confidence), (p1[0], p1[1] + 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 2)
+            
+            print("Confidence: " + name, confidence) 
 
     cv2.imshow('Video', smallFrame)
 
